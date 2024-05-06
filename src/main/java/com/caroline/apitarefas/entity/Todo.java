b19f3a35e8621223610196680f38a2ae17d3583b
Cahmarchi95 <cah.de.marchi@hotmail.com>
2024-05-06 12:23:26 -0300

    feat: add AllArgsConstructor and NoArgsConstructor

--- src/main/java/com/caroline/apitarefas/entity/Todo.java
+++ src/main/java/com/caroline/apitarefas/entity/Todo.java
@@ -1,11 +1,15 @@
 package com.caroline.apitarefas.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "todos")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
